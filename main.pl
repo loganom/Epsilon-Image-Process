85b27be90fd8813152f5f15dadca7c816ae8c25a
loganom <logan@mccamon.org>
2014-01-05 19:56:52 -0600

    use hash instead of array

--- main.pl
+++ main.pl
@@ -5,6 +5,7 @@ use warnings;
 
 use Cwd qw(abs_path);
 use Path::Class qw(file);
+use Data::Dumper qw(Dumper);
 
 use Text::CSV;
 my $csv = Text::CSV->new({ sep_char => ',' });
@@ -16,7 +17,27 @@ opendir (DIR, $img_directory) or die $!;
 
 # create arrays if we successfully opened the dir
 my @image_id; # array of image id numbers
-my @member_id; # array of member id numbers
+
+my $roster_file = "$directory/share/roster/roster.csv";
+my $list_file = "$directory/share/img/list/list.csv";
+
+my $roster_length = `wc -l $directory/share/roster/roster.csv`;
+my $list_length = `wc -l $directory/share/img/list/list.csv`;
+
+my $roster_line_count;
+my $list_line_count;
+
+if($roster_length =~ /^(\d+)(?:.*)$/) {
+	$roster_line_count = $1;
+}
+
+if($list_length =~ /^(\d+)(?:.*)$/) {
+	$list_line_count = $1;
+}
+
+if($roster_line_count != $list_line_count){
+	die "Verify that roster.csv and list.csv are the same length\n";
+}
 
 # get image file id number for all .JPG files in the /share/img/img 
 # directory that match our criteria
@@ -40,57 +61,47 @@ if($num_images == 0){
 	print "Images to process: $num_images\n";
 }
 
-my @list = read_list(); # list.csv data as an array
-my @roster = read_roster(); # list.csv data as an array
+# sort and create a new file in the same directory with sorted data for both files
+system("sort -d -k1 -t, $directory/share/roster/roster.csv > $directory/share/roster/roster_sorted.csv");
+system("sort -d -k1 -t, $directory/share/img/list/list.csv > $directory/share/img/list/list_sorted.csv");
+
+my %first = read_to_hash("$directory/share/roster/roster_sorted.csv");
+my %second = read_to_hash("$directory/share/img/list/list_sorted.csv");
+my %third = (%first,%second);
+
+create_dirs();
 
 # create_dirs create directories for new members 
 sub create_dirs {
-	foreach(@member_id) { #
+	foreach(@image_id) { #
 		my $error;
-		my $path = $directory . "/share/output/member/$_";
-		mkdir $path or $error = $!;
-		unless (-d $path) {
-		    die "Cannot create directory '$path': $error.";
+		my $path;
+		my $v = 0;
+		while($v < $roster_line_count) {
+			if($_ == $third{img_id}->[$v]){
+				$path = $directory . "/share/output/member/$third{id}->[$v]";
+				mkdir $path or $error = $!;
+				unless (-d $path) {
+				    die "Cannot create directory '$path': $error.";
+				}
+				system("mv $directory/share/img/img/IMG_$third{img_id}->[$v].JPG $path");
+			}
+			$v++;
 		}
 	}
 }
 
-# read_list create an array from list.csv
-# refactor read_list and read_roster to a single function 
-sub read_list {
-	my @list; # local array list
-	my $entry_counter = 0;
-
-	my $csv_data = $directory . "/share/img/list/list.csv";
-	open(my $data, '<', $csv_data) or die "Could not open '$csv' $!\n";
-	while(my $line = <$data>) { # while we have more data
-		chomp $line; 
-
-		if($csv->parse($line)) {
-			my @row = $csv->fields(); # create local array to hold data
-			push @{ $list[$entry_counter] }, @row; # add to local list array
-		}
-		$entry_counter++;
+sub read_to_hash {
+	open(INPUT,$_[0]) or die "ERR";
+	my @columns;
+	my %hash;
+	while(<INPUT>) {
+		chomp;
+		my $index=0;
+		push @{$columns[$index++]},$1 while /([^,]+)/g;
 	}
-	return @list; # return list
-}
-
-# read_roster create an array from roster.csv
-# refactor read_list and read_roster to a single function 
-sub read_roster {
-	my @roster; # local array roster
-	my $entry_counter = 0;
-
-	my $csv_data = $directory . "/share/roster/roster.csv";
-	open(my $data, '<', $csv_data) or die "Could not open '$csv' $!\n";
-	while(my $line = <$data>) { # while we have more data
-		chomp $line; 
-
-		if($csv->parse($line)) {
-			my @row = $csv->fields(); # create local array to hold data
-			push @{ $roster[$entry_counter] }, @row; # add to local roster array
-		}
-		$entry_counter++;
-	}
-	return @roster; # return roster
+	my @headings=map {shift @$_} @columns;
+	@hash{@headings}=@columns;
+	close(INPUT);
+	return %hash;
 }
